6d87ccb951ab22cde0d57a816fc41ccb15af0421
John-Magnus Johnsen <jom_jon@hotmail.com>
2025-09-27 18:33:10 +0200

    Update doc strings

--- Code/functions.py
+++ Code/functions.py
@@ -5,27 +5,27 @@ from sklearn.metrics import mean_squared_error
 
 def Polynomial_Features(x, p, intercept=False):
     """
+    Feature matrix for one variable.
     Take an array of x values, and the desired polynomial degree p.
-    Create a feature (design) matrix with first column x**1, second column with x**2, and so on. I.e. the i-th column containing x**(i+1).
-    Intercept=True will turn the first column into ones, meaning the i-th column will contain the value x**(j).
+    Create a feature (design) matrix with first column x**1, second column with x**2, and so on.
+    Intercept=True, will turn the first column into ones, meaning the 2nd column will contain the value x**(1) and so on.
     """
     n = len(x)
     if intercept  == True:
         X = np.zeros((int(n), int(p + 1)))
         for i in range(0, int(p+1)):
-            X[:, i] = x**i  # Create first column with only ones (since x**0 = 1), the intercept column
+            X[:, i] = x**i  # Create first column with only ones (since x**0 = 1), called the intercept or bias column
     else:
         X = np.zeros((int(n), int(p)))
         for i in range(0, int(p)):
             X[:, i] = x**(i+1)
-    
     return X
 
 def OLS_parameters(X, y):
     """
     The closed form solution of least square optimization.
     Take a feature matrix X, and a column vector y.
-    Return the parameters of OLS fit.
+    Return the beta parameters of the OLS fit.
     """
     # The inverse of matrix X, using .pinv in case X is not square
     # The beta vector in: X @ beta = y, solved for beta
@@ -36,16 +36,19 @@ def Ridge_parameters(X, y, ridge_lambda):
     """
     The closed form solution of Ridge optimization.
     Take a feature matrix X, and a column vector y, and the Ridge lambda parameter.
-    Return the parameters of the Ridge fit.
+    Return the beata parameters of the Ridge fit.
     """
     # Assumes X is scaled and has no intercept column
     I = np.identity(np.shape(X)[1])    # Create identity matrix same shape as X.T @ X, columns of X decide the shape, (nxm)(mxn)=(nxn)
-    # Element-wise multiplication with * 
-    return np.linalg.pinv(X.T @ X + ridge_lambda*I) @ X.T @ y
+    # Element-wise multiplication with *
+    beta = np.linalg.pinv(X.T @ X + ridge_lambda*I) @ X.T @ y
+    return beta
 
 def Find_MSE_Ridge_predict_poly_ridgelambda(x_data, y_data, poly_degree, ridge_lambda):
     """
-    Calculate the MSE
+    MSE for a spesifc configuration of poly_degree and ridge lambda.
+    Take x data and y data, poly_degree as desired polynomial degree of model, and the parameter ridge_lambda.
+    Return the MSE between between a test set of y and the model prediction trained on a train set of y. 
     """
     x = x_data
     y = y_data
